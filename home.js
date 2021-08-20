9a8039a6a16c84136ca1b4254c2dd16c24413ec0
Alvesx3 <mateusbitc@gmail.com>
2021-08-20 15:44:38 -0300

    front

---
+++ home.js
@@ -0,0 +1,36 @@
+const host = 'http://localhost/api';
+const endpoint = host + '/Lampada'
+
+function list(){
+    //criar instancia do objeto request
+    let request = new XMLHttpRequest();
+    //open
+    request.open('GET', endpoint);
+    //send
+    request.send();
+    //onload
+    request.onload = function (){
+        
+        let lampada = JSON.parse(this.responseText);
+        let tabela = document.getElementById("lampadatab");
+        let corpo = tabela.getElementsByTagName("tbody")[0];
+        corpo.innerHTML = "";
+        lampada.forEach(e => {
+            let linha = `<tr>
+                            <td>${e.id}</td>
+                            <td>${e.tipo}</td>
+                            <td>${e.watts}</td>
+                        </tr>`;
+            corpo.innerHTML += linha;
+        });
+    }
+}
+function Create(model){
+    let request = new XMLHttpRequest();
+request.open('POST',endpoint);
+request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+request.send(JSON.stringify(model));
+request.onload = function(){
+    
+    }
+}
